27f81c91c41e2d98a908c1695658d0bc95031a1b
minierm97 <minierm97@gmail.com>
2018-02-05 22:57:16 -0800

    Added the Zorb bootstrap gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  get 'site/index'
+
   devise_for :users
   resources :bookmarks
-  get 'welcome/homepage'
+  # get 'welcome/homepage'
 
-  root 'bookmarks#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # root 'bookmarks#index'
+  root 'site#index'
 end
